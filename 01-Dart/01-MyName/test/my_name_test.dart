f7b06d8be2920763c4d8fb8210c460287d4d2d96
Eduardo Piccin <ducu_yusuke@hotmail.com>
2022-06-02 18:22:02 -0300

    resolução de conflitos

--- 01-Dart/01-MyName/test/my_name_test.dart
+++ 01-Dart/01-MyName/test/my_name_test.dart
@@ -1,8 +1,5 @@
 import 'package:test/test.dart';
-<<<<<<< HEAD
-=======
 import '../lib/my_name.dart';
->>>>>>> cb7c29960e8b03e158ce3bc67e054b51092ba78d
 
 void main() {
   test('deve retornar uma string', () {
